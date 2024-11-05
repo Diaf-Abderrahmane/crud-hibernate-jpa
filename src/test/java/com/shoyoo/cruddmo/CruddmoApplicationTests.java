46bb3a2e3d9a13006ca3b9ca41481134cd19f012
Diaf Abderrahmane <ja_diaf@esi.dz>
2024-11-05 18:03:26 +0100

    Initial commit

---
+++ src/test/java/com/shoyoo/cruddmo/CruddmoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shoyoo.cruddmo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CruddmoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
